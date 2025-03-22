fe86bfd6fb1672dc360f8b14cc3a410e3c437b54
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-22 09:26:25 +0500

    File updated

--- Python_Assignment 01.py
+++ Python_Assignment 01.py
@@ -1,12 +1,10 @@
 #Question no 01 : Part 01
 #Reverse String
-'''
+#Method 01 : Using Function
+
 user_input_string = input("Enter a string: ")
 def reversed_string(string):
     return string[::-1] # start, stop and step.. step of -1 will start from backward direction.
-reversed_string(user_input_string)
-'''
-def reverse_string(strings):
-    return strings[::-1]  # Start , stop , step
-user_input = input("Enter a string: ") # Hello Python
-reverse_string(user_input)
+print("The reversed string is : " ,reversed_string(user_input_string))
+
+#Method 02 : Using For loop
