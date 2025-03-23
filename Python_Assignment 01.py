a6a035dccef13f06d9d9d555ac7c44edbbcd8896
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-23 06:16:54 +0500

    File Updated

--- Python_Assignment 01.py
+++ Python_Assignment 01.py
@@ -2,6 +2,7 @@
 #Reverse String
 #Method 01 : Using Function
 
+'''
 user_input_string = input("Enter a string: ")
 def reversed_string(string):
     return string[::-1] # start, stop and step.. step of -1 will start from backward direction.
@@ -13,4 +14,18 @@ reverse_user_string = ''
 for char in user_input_string:
     reverse_user_string = char + reverse_user_string
 print(f"User reversed string is: {reverse_user_string}")
-#nn
+
+'''
+
+#Question 01 : Part 02
+#Numbers of vowels in the string
+user_input_string = input("Enter a string: ")
+#Method 01
+for vowel in user_input_string:
+    if vowel in ['a','e','i','o','u']:
+        print(vowel)
+        
+#Method 02
+for vowel in user_input_string:
+    if vowel.lower() == "aeiou":
+        print(vowel)
