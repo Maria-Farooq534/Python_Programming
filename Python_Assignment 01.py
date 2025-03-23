d88875118948bb5aa3a027a3a68fe61a7274bb37
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-23 07:22:51 +0500

    File Updated

--- Python_Assignment 01.py
+++ Python_Assignment 01.py
@@ -2,35 +2,38 @@
 #Reverse String
 #Method 01 : Using Function
 
-'''
-user_input_string = input("Enter a string: ")
-def reversed_string(string):
-    return string[::-1] # start, stop and step.. step of -1 will start from backward direction.
-print("The reversed string is : " ,reversed_string(user_input_string))
+#
+#user_input_string = input("Enter a string: ")
+#def reversed_string(string):
+#    return string[::-1] # start, stop and step.. step of -1 will start from backward direction.
+#print("The reversed string is : " ,reversed_string(user_input_string))
+#
+##Method 02 : Using For loop
+#print("Reverse string using for loop: ")
+#reverse_user_string = ''
+#for char in user_input_string:
+#    reverse_user_string = char + reverse_user_string
+#print(f"User reversed string is: {reverse_user_string}")
 
-#Method 02 : Using For loop
-print("Reverse string using for loop: ")
-reverse_user_string = ''
-for char in user_input_string:
-    reverse_user_string = char + reverse_user_string
-print(f"User reversed string is: {reverse_user_string}")
 
-'''
 
 #Question 01 : Part 02
 #Numbers of vowels in the string
 user_input_string = input("Enter a string: ")
 #Method 01
+sum_of_vowels = ""
 for vowel in user_input_string:
     if vowel.lower() in ['a','e','i','o','u']:
-        print("Number of vowels: " , vowel)
+        print("vowels: " , vowel)
+        sum_of_vowels += vowel
+print("Number of vowels: ", len(sum_of_vowels))
         
 #Method 02
-for vowel in user_input_string:
-    if vowel.lower() in "aeiou":
-        print("The vowels: " , vowel)
-#Method 03 : Using a Function
-def check_vowels(string):
-    vowels = "aeiouAEIOU"
-    return sum(1 for char in string if char in vowels)   
-print("The vowels in the string: " , check_vowels(user_input_string))
+#for vowel in user_input_string:
+#    if vowel.lower() in "aeiou":
+#        print("The vowels: " , vowel)
+##Method 03 : Using a Function
+#def check_vowels(string):
+#    vowels = "aeiouAEIOU"
+#    return sum(1 for char in string if char in vowels)   
+#print("The vowels in the string: " , check_vowels(user_input_string))
