8c2d05e0c6e950aab316b10dd9f1fea1b3948a94
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-27 12:26:10 +0500

    File Updated

---
+++ Assignment 02 Q 01.py
@@ -0,0 +1,36 @@
+#For String
+
+user_input_string = input("Enter a string: ")
+
+user_input_integer = int(input("Enter an Integer: "))
+
+user_input_bool = bool(input("Enter a bool Value (True/False): "))
+
+user_input_float = float(input("Enter a float: "))
+
+print("Given string:" , user_input_string)
+print("Given Integer Value:" , user_input_integer)
+print("Given Boolean Value:" , user_input_bool)
+print("Given Float Value:" , user_input_float)
+
+#Converting it to uppercase
+
+print("Given string in uppercase:" , user_input_string.upper())
+
+# -------------------- For Integer -------------------------
+
+def number_check(number):
+    if number % 2 == 0:
+        print(f"{number} is an even number.")
+    else:
+        print(f"{number} is an odd number.")
+
+print(number_check("The number you have entered: ", user_input_integer))
+
+# ---------------------------- For Float ---------------------------
+
+def float_multiply(num):
+    return num*2
+
+
+print("New float Value: " , float_multiply(user_input_float))
