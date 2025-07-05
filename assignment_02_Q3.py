85f2792ecb032f2ab492edfea93712fe7cacf8e4
Maria-Farooq534 <mariafarooq164@gmail.com>
2025-07-04 21:59:19 -0700

    practice

---
+++ assignment_02_Q3.py
@@ -0,0 +1,43 @@
+# #Question 3: Loops
+# Write a Python program that:
+# 1. Accepts a list of integers from the user.
+# 2. Loops through the list and prints out each number.
+# 3. If a number is greater than 10, skips it using the continue statement.
+# 4. Stops the loop if the number is 20 using the break statement.
+# 5. After the loop ends, prints a message that the loop ended naturally.
+
+# # 1. Accepts a list of integers from the user.
+
+length_of_list = int(input("Enter length of list: "))
+
+integer_list = []
+
+for i in range(1, length_of_list + 1):
+    number = int(input(f"Enter number {i} : "))
+    integer_list.append(number)
+
+print(f"\nThe list of integers: {integer_list}")
+# # 2. Loops through the list and prints out each number.
+print(f"The numbers in {integer_list} are:\n")
+for num in integer_list:
+    print(num)
+
+
+    
+
+# # 3. If a number is greater than 10, skips it using the continue statement.
+print("Skipping numbers:")
+new_list = []
+for num in integer_list:
+    if num > 10 and num != 20:
+        print(f"Skip the number {num} as it is greater than 10.")
+        continue
+    new_list.append(num)
+print(f"\nNew list after skipping numbers greater than 10 is : {new_list}")
+
+# 4. Stops the loop if the number is 20 using the break statement.
+for num in new_list:
+    if num == 20:
+        print(f"Stop the loop as the number is 20")
+    break
+
